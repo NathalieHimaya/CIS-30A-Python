5d99749eabdcfcb605360e0a2983882f98603cf3
Nathalie Himaya <natramoshi@gmail.com>
2025-06-30 15:43:00 -0700

    start lab 2

---
+++ lab2.py
@@ -0,0 +1,10 @@
+#String Sequencing and Built-in Methods
+#Ex. 1
+
+name = "Nathalie"
+
+print(name[0]," ",name[7] )
+print("The number of characters is my name is ", len(name))
+print(name[0], " ", name[2])
+print(name[1], " ", name[7])
+print(len(name[6]))
